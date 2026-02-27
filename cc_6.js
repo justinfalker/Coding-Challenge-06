ee3ad7c6cb9e01bebb54037f74ff62c94b106cb9
Justin Falker <falkerj@usf.edu>
2026-02-27 10:35:57 -0500

    Completed Step 3 (used super)

--- cc_6.js
+++ cc_6.js
@@ -11,3 +11,16 @@ class Employee {
     return `${this.name} is an employee in the ${this.department} department.`;
   }
 }
+
+// Step 3
+
+class Manager extends Employee {
+  constructor(name, department, teamSize) {
+    super(name, department);
+    this.teamSize = teamSize;
+  }
+
+  describe() {
+    return `${this.name} is the manager of the ${this.department} department.`;
+  }
+}
