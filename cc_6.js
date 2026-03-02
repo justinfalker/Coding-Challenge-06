faace53c4f4ba739caced94232f53a142f418675
Justin Falker <falkerj@usf.edu>
2026-03-01 22:15:14 -0500

    Completed Steps 4 - 6

--- cc_6.js
+++ cc_6.js
@@ -21,6 +21,41 @@ class Manager extends Employee {
   }
 
   describe() {
-    return `${this.name} is the manager of the ${this.department} department.`;
+    return `${this.name} is the manager of the ${this.department} department. They lead a team of ${this.teamSize}.`;
   }
 }
+
+// Step 4
+
+const employee1 = new Employee("Austin", "Accounting");
+const employee2 = new Employee("Alexis", "Analytics");
+const manager1 = new Manager("Ashley", "IT", 4);
+const manager2 = new Manager("Sean", "Sales", 8);
+
+// Step 5
+
+class Company {
+  constructor() {
+    this.employees = [];
+  }
+
+  addEmployee(employee) {
+    this.employees.push(employee);
+  }
+
+  listEmployees() {
+    this.employees.forEach((employee) => {
+      console.log(employee.describe());
+    });
+  }
+}
+
+// Step 6
+
+const myCompany = new Company();
+myCompany.addEmployee(employee1);
+myCompany.addEmployee(employee2);
+myCompany.addEmployee(manager1);
+myCompany.addEmployee(manager2);
+
+myCompany.listEmployees();
